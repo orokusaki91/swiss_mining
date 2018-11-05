0a9ffdc2ee040569bdd76ffb97ec4d33e6a3741f
Ivan Jovkovic <jokac14@gmail.com>
2018-11-05 14:44:10 +0100

    particles

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,9 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'json/particles.json', function () {
+particlesJS.load('particles-js', 'particles.json', function () {
 	console.log('callback - particles.js config loaded');
 });
 
-particlesJS.load('particles-js2', 'json/particles.json', function () {
+particlesJS.load('particles-js2', 'particles.json', function () {
 	console.log('callback - particles.js config loaded');
 });
 
