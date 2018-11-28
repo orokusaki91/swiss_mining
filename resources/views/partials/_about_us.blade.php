623c475ec701d71409917e094918f2db47ab0161
Ivan Jovkovic <jokac14@gmail.com>
2018-11-28 16:39:18 +0100

    fix issue with css

--- resources/views/partials/_about_us.blade.php
+++ resources/views/partials/_about_us.blade.php
@@ -32,9 +32,9 @@
                             <div class="col-md-3 wow fadeInUp">
                                 <div class="item">
                                     <div class="icon">
-                                        <img class="icon-img" src="{{ asset("img/mission-vision/item-1.png") }}" />
-                                        <img class="visible" src="{{ asset("img/mission-vision/item-1-visible.png") }}" />
-                                        <img class="glow" src="{{ asset("img/mission-vision/item-1-glow.png")}}" />
+                                        <img class="icon-img" src="{{ asset('img/mission-vision/item-1.png') }}" />
+                                        <img class="visible" src="{{ asset('img/mission-vision/item-1-visible.png') }}" />
+                                        <img class="glow" src="{{ asset('img/mission-vision/item-1-glow.png')}}" />
                                     </div>
                                     <div class="info">
                                         <h2>01.</h2>
@@ -50,9 +50,9 @@
                             <div class="col-md-3 wow fadeInUp" data-wow-delay=".1s">
                                 <div class="item two">
                                     <div class="icon">
-                                        <img class="icon-img" src="{{ asset("img/mission-vision/item-2.png") }}" />
-                                        <img class="visible" src="{{ asset("img/mission-vision/item-2-visible.png") }}" />
-                                        <img class="glow" src="{{ asset("img/mission-vision/item-2-glow.png") }}" />
+                                        <img class="icon-img" src="{{ asset('img/mission-vision/item-2.png') }}" />
+                                        <img class="visible" src="{{ asset('img/mission-vision/item-2-visible.png') }}" />
+                                        <img class="glow" src="{{ asset('img/mission-vision/item-2-glow.png') }}" />
                                     </div>
                                     <div class="info">
                                         <h2>02.</h2>
@@ -86,9 +86,9 @@
                             <div class="col-md-3 wow fadeInUp" data-wow-delay=".3s">
                                 <div class="item four">
                                     <div class="icon">
-                                        <img class="icon-img" src="{{ asset("img/mission-vision/item-4.png") }}" />
-                                        <img class="visible" src="{{ asset("img/mission-vision/item-4-visible.png") }}" />
-                                        <img class="glow" src="{{ asset("img/mission-vision/item-4-glow.png") }}" />
+                                        <img class="icon-img" src="{{ asset('img/mission-vision/item-4.png') }}" />
+                                        <img class="visible" src="{{ asset('img/mission-vision/item-4-visible.png') }}" />
+                                        <img class="glow" src="{{ asset('img/mission-vision/item-4-glow.png') }}" />
                                     </div>
                                     <div class="info">
                                         <h2>04.</h2>
@@ -115,7 +115,7 @@
             <!-- Particles-roadmap start -->
             <div id="particles-roadmap" class="particles"></div>
             <!-- Particles-roadmap end -->
-            <img class="roadmap-image" src="{{ asset("img/roadmap/roadmap.png") }}" alt="img/roadmap/roadmap.png" />
+            <img class="roadmap-image" src="{{ asset('img/roadmap/roadmap.png') }}" alt="img/roadmap/roadmap.png" />
         </div>
     </div>
     <!-- Roadmap end -->
@@ -123,7 +123,7 @@
     <div id="roadmap-controls" class="d-md-none">
         <div class="container-fluid">
             <div class="text-center">
-                <img class="scroll-icon" src="{{ asset("img/roadmap/scroll.png") }}" alt="img/roadmap/scroll.png" />
+                <img class="scroll-icon" src="{{ asset('img/roadmap/scroll.png') }}" alt="img/roadmap/scroll.png" />
             </div>
             <div class="row no-gutters">
                 <div class="col-6 text-right">
@@ -155,10 +155,10 @@
                                         <div class="item">
                                             <div class="profile">
                                                 <div class="profile-img">
-                                                    <img src="{{ asset("img/team/profile.png") }}" alt="img/team/profile.png" />
+                                                    <img src="{{ asset('img/team/profile.png') }}" alt="img/team/profile.png" />
                                                 </div>
                                                 <button class="btn">
-                                                    <img src="{{ asset("img/team/linkedin.png") }}" class="linkedin" alt="img/team/linkedin.png" />
+                                                    <img src="{{ asset('img/team/linkedin.png') }}" class="linkedin" alt="img/team/linkedin.png" />
                                                 </button>
                                             </div>
                                             <div class="info">
