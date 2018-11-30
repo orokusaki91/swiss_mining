a9e1c16e56dad8dd3ba640935716f780277ca9f3
orokusaki91 <31542903+orokusaki91@users.noreply.github.com>
2018-11-30 17:40:53 +0100

    fix scripts

--- resources/views/partials/_scripts.blade.php
+++ resources/views/partials/_scripts.blade.php
@@ -1,11 +1,11 @@
-<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
-<script type="text/javascript" src="js/popper.min.js"></script>
-<script type="text/javascript" src="js/bootstrap.min.js"></script>
-<script type="text/javascript" src="js/owl.carousel.min.js"></script>
-<script type="text/javascript" src="js/particles.min.js"></script>
-<script type="text/javascript" src="js/wow.min.js"></script>
-<script type="text/javascript" src="js/wow.js"></script>
-<script type="text/javascript" src="js/all.min.js"></script>
+<script type="text/javascript" src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/popper.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/bootstrap.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/owl.carousel.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/particles.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/wow.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/wow.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/all.min.js') }}"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.19.0/sweetalert2.all.min.js"></script>
-<script type="text/javascript" src="js/all.min.js"></script>
+<script type="text/javascript" src="{{ asset('js/all.min.js') }}"></script>
 <script type="text/javascript" src="{{ asset('js/main.js?ver=' . str_random(5)) }}"></script>
