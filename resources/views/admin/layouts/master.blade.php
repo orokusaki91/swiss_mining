4333c5ea664decb8c9682a038516446711ea0baa
Ivan Jovkovic <jokac14@gmail.com>
2018-11-30 17:27:25 +0100

    admin panel

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -1,3 +1,8 @@
-@include('admin.layouts.nav')
-
-@yield('main')
+@include('partials._header')
+<div id="vertical-menu">
+	@include('admin.layouts.nav')
+</div>
+<div id="admin-content">
+	@yield('main')
+</div>
+@include('partials._scripts')
--- resources/views/admin/layouts/nav.blade.php
+++ resources/views/admin/layouts/nav.blade.php
@@ -1,1 +1,26 @@
-<a href="{{ route('admin.aboutUs') }}">About us</a>
+<ul id="admin-nav">
+	<li>
+		<a href="{{ route('admin.aboutUs') }}" data-toggle="collapse" href="#dropdown-about" role="button" aria-expanded="false" aria-controls="dropdown-about">About us</a>
+		<i class="fas fa-arrow-circle-right"></i>
+		<ul id="dropdown-about" class="dropdown collapse">
+			<li>
+				<a href="{{ route('admin.aboutUs') }}">Team</a>
+			</li>
+		</ul>
+	</li>
+	<li>
+		<a href="{{ route('admin.aboutUs') }}">Services</a>
+		<i class="fas fa-arrow-circle-right"></i>
+		<ul id="dropdown-services" class="dropdown collapse">
+			<li>
+				<a href="javasript:void(0)">Swissmining Solutions</a>
+			</li>
+			<li>
+				<a href="javasript:void(0)">Swiss Turbo Token</a>
+			</li>
+		</ul>
+	</li>
+	<li>
+		<a href="{{ route('admin.aboutUs') }}">Partners</a>
+	</li>
+</ul>
