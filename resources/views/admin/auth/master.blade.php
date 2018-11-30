c74e028417dc01ed7daaf16cf1c9e6720f9a5652
Ivan Jovkovic <jokac14@gmail.com>
2018-11-30 18:48:33 +0100

    login style

--- resources/views/admin/auth/master.blade.php
+++ resources/views/admin/auth/master.blade.php
@@ -13,11 +13,12 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+    <link rel="dns-prefetch" href="//fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css" />
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
+    @include('partials._header')
 </head>
 <body>
     <div id="app">
