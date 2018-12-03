73876a141c75aab1f7b4a2a276f7e5e1545a2cb9
Igor Nikolic <igorn995@gmail.com>
2018-12-03 16:34:39 +0100

    admin panel, active class for vertical menu

--- resources/views/admin/layouts/nav.blade.php
+++ resources/views/admin/layouts/nav.blade.php
@@ -1,23 +1,26 @@
 <ul id="admin-nav">
 	<li>
-		<a class="active" href="{{ route('admin.home') }}">Home</a>
+		<a href="{{ route('admin.home') }}">Home</a>
 	</li>
 	<li>
-		<a href="{{ route('admin.aboutUs') }}">About us</a>
+		<a {{ Request::segment(2) == 'about-us' ? "class=active" : '' }} href="{{ route('admin.aboutUs') }}">About us</a>
 	</li>
 	<li>
-		<a href="{{ route('admin.missionVision') }}">Mission and Vision</a>
+		<a {{ Request::segment(2) == 'mission-vision' ? "class=active" : '' }} href="{{ route('admin.missionVision') }}">Mission and Vision</a>
 	</li>
 	<li>
-		<a href="{{ route('admin.team') }}">Team</a>
+		<a {{ Request::segment(2) == 'team' ? "class=active" : '' }} href="{{ route('admin.team') }}">Team</a>
 	</li>
 	<li>
-		<a href="{{ route('admin.services') }}">Services</a>
+		<a {{ Request::segment(2) == 'services' ? "class=active" : '' }} href="{{ route('admin.services') }}">Services</a>
 	</li>
 	<li>
-		<a href="{{ route('admin.solutions') }}">Solutions</a>
+		<a {{ Request::segment(2) == 'solutions' ? "class=active" : '' }} href="{{ route('admin.solutions') }}">Solutions</a>
+	</li>
 	<li>
+		<a {{ Request::segment(2) == 'turbo-token' ? "class=active" : '' }} href="{{ route('admin.solutions') }}">Turbo token</a>
+	</li>
 	<li>
-		<a href="{{ route('admin.newsletter') }}">Newsletters</a>
+		<a {{ Request::segment(2) == 'newsletter' ? "class=active" : '' }} href="{{ route('admin.newsletter') }}">Newsletters</a>
 	</li>
 </ul>
