a7454ae93bb4ee50e4a29b41f5809e837b28729c
Ivan Jovkovic <jokac14@gmail.com>
2018-12-03 16:25:04 +0100

    active a href vertical menu

--- resources/views/admin/layouts/nav.blade.php
+++ resources/views/admin/layouts/nav.blade.php
@@ -1,56 +1,23 @@
 <ul id="admin-nav">
 	<li>
-		<a href="{{ route('admin.home') }}">Home</a>
-		<button class="btn" type="button" data-toggle="collapse" data-target="#dropdown-about" aria-expanded="false" aria-controls="dropdown-about">
-			<i class="fas fa-arrow-circle-right" ></i>
-		</button>
+		<a class="active" href="{{ route('admin.home') }}">Home</a>
 	</li>
 	<li>
 		<a href="{{ route('admin.aboutUs') }}">About us</a>
-		<button class="btn" type="button" data-toggle="collapse" data-target="#dropdown-about" aria-expanded="false" aria-controls="dropdown-about">
-			<i class="fas fa-arrow-circle-right" ></i>
-		</button>
 	</li>
 	<li>
 		<a href="{{ route('admin.missionVision') }}">Mission and Vision</a>
-		<button class="btn" type="button" data-toggle="collapse" data-target="#dropdown-about" aria-expanded="false" aria-controls="dropdown-about">
-			<i class="fas fa-arrow-circle-right" ></i>
-		</button>
 	</li>
 	<li>
 		<a href="{{ route('admin.team') }}">Team</a>
-		<button class="btn" type="button" data-toggle="collapse" data-target="#dropdown-about" aria-expanded="false" aria-controls="dropdown-about">
-			<i class="fas fa-arrow-circle-right" ></i>
-		</button>
 	</li>
 	<li>
 		<a href="{{ route('admin.services') }}">Services</a>
-		<button class="btn" type="button" data-toggle="collapse" data-target="#dropdown-services" aria-expanded="false" aria-controls="dropdown-services">
-			<i class="fas fa-arrow-circle-right" ></i>
-		</button>
 	</li>
 	<li>
 		<a href="{{ route('admin.solutions') }}">Solutions</a>
-		<button class="btn" type="button" data-toggle="collapse" data-target="#dropdown-services" aria-expanded="false" aria-controls="dropdown-services">
-			<i class="fas fa-arrow-circle-right" ></i>
-		</button>
 	<li>
-	<li>
-		<a href="{{ route('admin.turboToken') }}">Turbo Token</a>
-		<button class="btn" type="button" data-toggle="collapse" data-target="#dropdown-services" aria-expanded="false" aria-controls="dropdown-services">
-			<i class="fas fa-arrow-circle-right" ></i>
-		</button>
-	</li>
-	<li>
-		<a href="{{ route('admin.partners') }}">Partners</a>
-		<button class="btn" type="button" data-toggle="collapse" data-target="#dropdown-services" aria-expanded="false" aria-controls="dropdown-services">
-			<i class="fas fa-arrow-circle-right" ></i>
-		</button>
-	</li>
 	<li>
 		<a href="{{ route('admin.newsletter') }}">Newsletters</a>
-		<button class="btn" type="button" data-toggle="collapse" data-target="#dropdown-services" aria-expanded="false" aria-controls="dropdown-services">
-			<i class="fas fa-arrow-circle-right" ></i>
-		</button>
 	</li>
 </ul>
