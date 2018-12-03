60aac45dbac121d6fcd2182cca97a1aaeab94245
Igor Nikolic <igornikolic77@yahoo.com>
2018-12-03 14:51:03 +0100

    edit mission vision modify

--- resources/views/admin/partials/missionVision/edit.blade.php
+++ resources/views/admin/partials/missionVision/edit.blade.php
@@ -7,26 +7,22 @@
     @csrf
 
     <label for="body">Description - EN</label><br>
-    <textarea name="body_en">{!! $vision->body_en !!}</textarea>
+    <textarea name="body_en" cols="30" rows="10">{!! $vision->body_en !!}</textarea>
     
     <label for="body">Description - DE</label><br>
-    <textarea name="body_de">{!! $vision->body_de !!}</textarea>
+    <textarea name="body_de" cols="30" rows="10">{!! $vision->body_de !!}</textarea>
     
     <label for="body">Description - FR</label><br>
-    <textarea name="body_fr">{!! $vision->body_fr !!}</textarea>
+    <textarea name="body_fr" cols="30" rows="10">{!! $vision->body_fr !!}</textarea>
     
     <label for="body">Description - IT</label><br>
-    <textarea name="body_it">{!! $vision->body_it !!}</textarea>
+    <textarea name="body_it" cols="30" rows="10">{!! $vision->body_it !!}</textarea>
     
     <label for="body">Description - ES</label><br>
-    <textarea name="body_es">{!! $vision->body_es !!}</textarea>
+    <textarea name="body_es" cols="30" rows="10">{!! $vision->body_es !!}</textarea><br>
 
     <input type="submit" value="Change content">
 
     @include('admin.errors')
 </form>
-@endsection
-
-@section('scripts')
-    @include('admin.scripts')
 @endsection
